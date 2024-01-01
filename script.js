8dcda1a6080c3c9ea88da085ea517d81aa8c3361
Amogelang Korase <153375292+AmogelangKorase04@users.noreply.github.com>
2024-01-01 13:11:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipletitle",{
+    strings:[" an Aspiring Full-Stack Develeoper","a Computer Sciences student"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:5,
+    loop: true
+})
